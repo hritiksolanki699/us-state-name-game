c54a57dd5df69809e3c65adc53e23da2053c438a
Hritik <hritiksolanki699@gmail.com>
2022-06-07 10:05:34 +0530

    list comprehension

--- main.py
+++ main.py
@@ -16,10 +16,7 @@ while len(guessed_states) < 51:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="What's another state's name?").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv("State to learn.csv")
         break
